0ed2761150d453a7d724894819f1f7156bedeef2
Greg Wilding <gwilding@possneuro.com>
2024-09-25 14:29:57 -0600

    Camera moves with scenes
    - This implementation is rubbish, but you get the idea

--- Boccia-Unity/Assets/Boccia/UI/ScreenSwitcher.cs
+++ Boccia-Unity/Assets/Boccia/UI/ScreenSwitcher.cs
@@ -7,10 +7,12 @@ using UnityEngine;
 //
 public class ScreenSwitcher : MonoBehaviour
 {
+    public Camera ScreenCamera;
     public GameObject StartMenu;
     public GameObject PlayMenu;
 
     private BocciaModel model;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,20 +29,24 @@ public class ScreenSwitcher : MonoBehaviour
     {
         switch (model.CurrentScreen)
         {
-            case BocciaScreen.StartMenu:
-                StartMenu.SetActive(true);
-                PlayMenu.SetActive(false);
-                break;
-
+            // TODO - make a helper function hide all but given screen and move camera to it
             case BocciaScreen.PlayMenu:
                 StartMenu.SetActive(false);
-                PlayMenu.SetActive(true);
+                PlayMenu.SetActive(true); 
+                MoveCamera(new Vector3(849, 255, -184), new Vector3(7, 4, 0));
                 break;
             
             default:
                 StartMenu.SetActive(true);
                 PlayMenu.SetActive(false);
+                MoveCamera(new Vector3(121, 138, 31), new Vector3(0, 18, 0));
                 break;
         }
+    }
+
+    private void MoveCamera(Vector3 position, Vector3 rotation)
+    {
+        ScreenCamera.transform.position = position;
+        ScreenCamera.transform.rotation = Quaternion.Euler(rotation);
     }
 }
