eb82281cbb14ee975639d85d375d75e1421cfcf2
Daniel Comaduran <dcomadur@ucalgary.ca>
2024-10-02 15:46:20 -0600

    Fix orientation with respect to base or rails rotation

--- Boccia-Unity/Assets/Boccia/BCI/FanPresenter.cs
+++ Boccia-Unity/Assets/Boccia/BCI/FanPresenter.cs
@@ -7,12 +7,19 @@ public class FanPresenter : MonoBehaviour
 {
     public FanGenerator fanGenerator;
 
+    public enum PositioningMode
+    {
+        None,
+        CenterToRails,
+        CenterToBase
+    }
+
     [Header("Positioning")]
-    public bool useRampPosition;
-    
+    public PositioningMode positioningMode;
+
     private BocciaModel _model;
     private Quaternion _originalRotation;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,27 +27,66 @@ public class FanPresenter : MonoBehaviour
         _originalRotation = transform.rotation;       
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void setFanPositionToRamp()
+    /// <summary>
+    /// Center the fan to the rails of the ramp. The fan will be generated
+    /// as stated in `CenterToOrigin`, and then an offset of half the angle
+    /// theta + the angle between the rails and their starting position
+    /// (i.e., with 0 deg over the middle leg od the ramp) will be applied
+    /// in the counter-clockwise direction.
+    /// </summary>
+    private void CenterToRails()
     {
         float shaftOrientation = _model.GetRampOrientation();
+        Debug.Log("Shaft orientation: " + shaftOrientation);
+        float zOffset = (180 - fanGenerator.theta)/2 - shaftOrientation;
         Quaternion newRotation = Quaternion.Euler(
             _originalRotation.eulerAngles.x,
-            _originalRotation.eulerAngles.y + shaftOrientation,
-            _originalRotation.eulerAngles.z
+            _originalRotation.eulerAngles.y,
+            _originalRotation.eulerAngles.z + zOffset
             );
         transform.rotation = newRotation;
     }
 
+    /// <summary>
+    /// Center the fan to the base of the ramp. The fan will be generated
+    /// as stated in `CenterToOrigin`, and then an offset of half the angle
+    /// theta will be applied in the counter-clockwise direction.
+    /// </summary>
+    private void CenterToBase()
+    {
+        float zOffset = (180 - fanGenerator.theta)/2;
+        Quaternion newRotation = Quaternion.Euler(
+            _originalRotation.eulerAngles.x,
+            _originalRotation.eulerAngles.y,
+            _originalRotation.eulerAngles.z + zOffset
+        );
+        transform.rotation = newRotation;
+    }
+
+    /// <summary>
+    /// Reset the fan to the original rotation. The fan will be generated
+    /// in the XY plane, counting the degrees in counter clock mode from
+    /// the +X axis.
+    /// </summary>
+    private void CenterToOrigin()
+    {
+        transform.rotation = _originalRotation;
+    }
 
     public void GenerateFan()
     {
-        if (useRampPosition) { setFanPositionToRamp(); }
+        // Reset to original rotation to avoid cumulative effects
+        CenterToOrigin();
+        
+        switch (positioningMode)
+        {
+            case PositioningMode.CenterToRails:
+                CenterToRails();
+                break;
+            case PositioningMode.CenterToBase:
+                CenterToBase();
+                break;
+        }
 
         fanGenerator.DestroyFanSegments();
         fanGenerator.GenerateFanShape();
