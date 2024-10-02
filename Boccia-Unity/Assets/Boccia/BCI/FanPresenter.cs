b4e29fba31d1f3e0757a0328daf7964ef412d4f7
Daniel Comaduran <dcomadur@ucalgary.ca>
2024-10-02 11:33:34 -0600

    Fan presenter to interact with model and orient fan based on ramp rotation

--- Boccia-Unity/Assets/Boccia/BCI/FanPresenter.cs
+++ Boccia-Unity/Assets/Boccia/BCI/FanPresenter.cs
@@ -10,14 +10,14 @@ public class FanPresenter : MonoBehaviour
     [Header("Positioning")]
     public bool useRampPosition;
     
-    private BocciaModel model;
+    private BocciaModel _model;
+    private Quaternion _originalRotation;
     
     // Start is called before the first frame update
     void Start()
     {
-        model = BocciaModel.Instance;        
-
-        if (useRampPosition) { setFanPositionToRamp(); } 
+        _model = BocciaModel.Instance; 
+        _originalRotation = transform.rotation;       
     }
 
     // Update is called once per frame
@@ -28,15 +28,21 @@ public class FanPresenter : MonoBehaviour
 
     public void setFanPositionToRamp()
     {
-        float shaftOrientation = model.GetRampOrientation();
-        Quaternion currentRotation = transform.rotation;
-        Quaternion newRotation = Quaternion.Euler(currentRotation.eulerAngles.x, shaftOrientation-90, currentRotation.eulerAngles.z);
+        float shaftOrientation = _model.GetRampOrientation();
+        Quaternion newRotation = Quaternion.Euler(
+            _originalRotation.eulerAngles.x,
+            _originalRotation.eulerAngles.y + shaftOrientation,
+            _originalRotation.eulerAngles.z
+            );
         transform.rotation = newRotation;
     }
 
 
     public void GenerateFan()
     {
+        if (useRampPosition) { setFanPositionToRamp(); }
+
+        fanGenerator.DestroyFanSegments();
         fanGenerator.GenerateFanShape();
     }
 }
