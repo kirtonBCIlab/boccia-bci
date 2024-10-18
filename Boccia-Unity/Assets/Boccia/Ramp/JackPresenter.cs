f37a3ceb08a581ef44cf67d93c80e3112f65b0b5
e.kinney-lang <ekinneyL44@gmail.com>
2024-10-18 09:11:40 -0600

    Cleaned up the scripts

--- Boccia-Unity/Assets/Boccia/Ramp/JackPresenter.cs
+++ Boccia-Unity/Assets/Boccia/Ramp/JackPresenter.cs
@@ -8,17 +8,12 @@ public class JackPresenter : MonoBehaviour
     public GameObject jackBall; // The ball prefab to use as the jack
     public GameObject spawnArea; // The area in which to spawn the jack
 
-    //This is now handled by the SpawnArea script
-    // private Vector2 xRange = new Vector2(-6f, 6f);
-    // private float yPosition = 0f;
-    // private Vector2 zRange = new Vector2(6f, 18f);
-
     // Start is called before the first frame update
     void Start()
     {
         model = BocciaModel.Instance;
         model.NewRandomJack += ModelChanged;
-        spawnArea = GameObject.FindWithTag("JackSpawnArea");
+        
     }
  
     private void OnDisable()
@@ -39,6 +34,8 @@ public class JackPresenter : MonoBehaviour
             Destroy(currentJack);
         }
 
+        spawnArea = GameObject.FindWithTag("JackSpawnArea");
+
         Vector3 randomJackPosition = spawnArea.GetComponent<SpawnArea>().ReturnRandomPosition();
         //Debug.Log(randomJackPosition);
 
@@ -46,27 +43,5 @@ public class JackPresenter : MonoBehaviour
         newJack.name = "JackBall";
 
     }
-
-    // private Vector3 RandomLocation()
-    // {
-    //     float xPosition = Random.Range(xRange.x, xRange.y);
-    //     float zPosition = Random.Range(zRange.x, zRange.y);
-        
-    //     return new Vector3(xPosition, yPosition, zPosition);
-    // }
-
-    // If the jack rolls out of bounds, reset it
-    // Note: the jack should not instantiate outside the boundary, but the Boccia ball could
-    // knock the jack out of bounds
-    /*
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("JackBall"))
-        {
-            Debug.Log("Jack out of bounds");
-            NewJack();
-        }
-    }
-    */
 
 }
