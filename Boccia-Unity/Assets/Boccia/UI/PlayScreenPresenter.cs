0e9682c437cb37fd4f68fac074e2211ee48c459d
Daniel Comaduran <dcomadur@ucalgary.ca>
2024-11-07 12:08:35 -0700

    Calculate random ball drop position and execute

--- Boccia-Unity/Assets/Boccia/UI/PlayScreenPresenter.cs
+++ Boccia-Unity/Assets/Boccia/UI/PlayScreenPresenter.cs
@@ -6,17 +6,22 @@ using UnityEngine.UI;
 public class PlayScreenPresenter : MonoBehaviour
 {
     public Button resetRampButton;
-    //public Button randomBallButton;
+    public Button randomBallButton;
 
     private BocciaModel _model;
+
+    private int _randomRotation;
+    private int _randomElevation;
 
     // Start is called before the first frame update
     void Start()
     {
-        // _model = BocciaModel.Instance;
+        _model = BocciaModel.Instance;
 
         // connect buttons to model
         resetRampButton.onClick.AddListener(_model.ResetRampPosition);
+        randomBallButton.onClick.AddListener(SetRandomBallDropPosition);
+        
         // TODO: see task BOC-90 for what the random ball button should do
     }
 
@@ -43,5 +48,16 @@ public class PlayScreenPresenter : MonoBehaviour
     private void ModelChanged()
     {
 
+    }
+
+    private void SetRandomBallDropPosition()
+    {
+        // Generate random values for the ball drop position
+        // add +1 to max to maxe it inclusive
+        // TODO: The min and max values should come from the model, not hard coded
+        _randomRotation = Random.Range(5, 181);
+        _randomElevation = Random.Range(1, 101);
+
+        _model.RandomBallDrop(_randomRotation, _randomElevation);
     }
 }
