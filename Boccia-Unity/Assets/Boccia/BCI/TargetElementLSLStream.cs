3cb09bdcf014485875f941eb5ed3a095548b2fed
daniellarbourque <daniellarosebourque@gmail.com>
2025-03-03 14:08:23 -0700

    Make sure objectID is set and also send selectable pool index

--- Boccia-Unity/Assets/Boccia/BCI/TargetElementLSLStream.cs
+++ Boccia-Unity/Assets/Boccia/BCI/TargetElementLSLStream.cs
@@ -12,7 +12,7 @@ public class TargetElementLSLStream : MonoBehaviour
     private string StreamType = "Text";
     public string StreamId = "target_element_stream_01";
     private StreamInfo _streamInfo;
-    private string _sample;
+    private string[] _sample;
     private BocciaModel _model;
     // Start is called before the first frame update
     void Start()
@@ -33,8 +33,10 @@ public class TargetElementLSLStream : MonoBehaviour
             return false;
         }
 
-        _streamInfo = new StreamInfo(StreamName, StreamType, 1, 0.0, LSL.channel_format_t.cf_string, StreamId);
+        _streamInfo = new StreamInfo(StreamName, StreamType, 2, 0.0, LSL.channel_format_t.cf_string, StreamId);
         _outlet = new StreamOutlet(_streamInfo);
+
+        _sample = new string[2];
 
         return true;
     }
@@ -60,17 +62,36 @@ public class TargetElementLSLStream : MonoBehaviour
         // Get the target element SPO currently stored in the model
         SPO targetSPO = _model.TargetElementSPO;
 
-        if (targetSPO != null)
+        if (targetSPO == null)
         {
-            // Get the object ID of the target element
-            _sample = targetSPO.ObjectID.ToString();
-
-            // Send the sample to the LSL stream
-            _outlet.push_sample(new string[] { _sample });
-            // Debug.Log("Target element object ID sent to LSL stream: " + _sample);
-
-            // Clear the target element SPO in the model
-            _model.ClearTargetElement();
+            return;
         }
+
+        // Make sure to wait for the object ID to be set
+        float timer = 0;
+        while (targetSPO.ObjectID == -100 && timer < 5)
+        {
+            timer += Time.deltaTime;
+        }
+        if (targetSPO.ObjectID == -100)
+        {
+            // Show warning if object ID still not set after 5 seconds
+            Debug.LogWarning("Object ID not set yet.");
+        }
+
+        // Get the object ID and selectable pool index of the target element
+        string objectID = targetSPO.ObjectID.ToString();
+        string selectablePoolIndex = targetSPO.SelectablePoolIndex.ToString();
+        
+        _sample[0] = "ObjectID: " + objectID;
+        _sample[1] = "iSPO: " + selectablePoolIndex;
+
+        // Send the sample to the LSL stream
+        _outlet.push_sample(_sample);
+        // Debug.Log("Target element object ID sent to LSL stream: " + _sample[0]);
+        // Debug.Log("Target element selectable pool index sent to LSL stream: " + _sample[1]);
+
+        // Clear the target element SPO in the model
+        _model.ClearTargetElement();
     }
 }
