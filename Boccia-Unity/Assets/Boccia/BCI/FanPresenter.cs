7b659b0e2effc416f2e11e82d226fa85170e9097
Daniel Comaduran <dcomadur@ucalgary.ca>
2025-03-11 10:34:42 -0600

    Change positioning of fan to ResetFanWhenPlayModeChanges()

--- Boccia-Unity/Assets/Boccia/BCI/FanPresenter.cs
+++ Boccia-Unity/Assets/Boccia/BCI/FanPresenter.cs
@@ -139,12 +139,6 @@ public class FanPresenter : MonoBehaviour
                 CenterGameOptionsMenu();
                 fanGenerator.GenerateFanShape(_fineFan);
         }
-        // Make sure the fan positioning mode is Center North for the training fan
-        if (fanTypeScreen == BocciaScreen.TrainingScreen)
-        {
-            positioningMode = FanPositioningMode.CenterNorth;
-            StartCoroutine(GenerateFanCoroutine());
-        }
         // For all other cases, generate fan with a coroutine
         else
         {
@@ -204,7 +198,7 @@ public class FanPresenter : MonoBehaviour
     }
 
     /// <summary>
-    ///  Resets the fan to generate a coarse fan if the gameMode changes
+    ///  Resets the fan to generate a coarse fan if the gameMode changes, and a north fan in training
     /// </summary>
     private void ResetFanWhenPlayModeChanges()
     {
@@ -215,6 +209,10 @@ public class FanPresenter : MonoBehaviour
         {
             positioningMode = FanPositioningMode.CenterToBase;
             _lastPlayMode = currentPlayMode;
+        }
+        else if (!currentlyInPlayMode && fanTypeScreen == BocciaScreen.TrainingScreen)
+        {
+            positioningMode = FanPositioningMode.CenterNorth;
         }
     }
 
